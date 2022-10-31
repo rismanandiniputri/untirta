b76f4c4fcc8ae00e3190b6f5a030272c70687821
rismanandini <rismanandiniputri1209@gmail.com>
2022-10-31 18:27:05 +0700

    upgrade

--- untirta/untirta/urls.py
+++ untirta/untirta/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from fkip.views import Fkip
+from fkip.views import fkip
+from fkip.views import *
 from feb.views import Feb
 from faperta.views import Faperta
 from fk.views import Fk
@@ -29,7 +30,7 @@ from pascasarjana.views import Pascasarjana
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Univ, name="univ"),
-    path('fkip/', Fkip, name="fkip"),
+    path('fkip/', fkip, name="fkip"),
     path('feb/', Feb, name="feb"),
     path('faperta/', Faperta, name="faperta"),
     path('fk/', Fk, name="fk"),
@@ -37,5 +38,11 @@ urlpatterns = [
     path('fisip/', Fisip, name="fisip"),
     path('ft/', Ft, name="ft"),
     path('pascasarjana/', Pascasarjana, name="pascasarjana"),
-
+    path('tambah-dosen/', tambah_dosen, name='tambah_dosen'),
+    path('dosen/ubah/<int:id_dosen>', ubah_dosen, name='ubah_dosen'),
+    path('dosen/hapus/<int:id_dosen>', hapus_dosen, name='hapus_dosen'),
+    path('tambah-staff//', tambah_staff, name='tambah_staff'),
+    path('staff/ubah/<int:id_staff>', ubah_staff, name='ubah_staff'),
+    path('tambah-mahasiswa/', tambah_mahasiswa, name='tambah_mahasiswa'),
+    path('mahasiswa/ubah/<int:id_mahasiswa>', ubah_mahasiswa, name='ubah_mahasiswa'),
 ]
